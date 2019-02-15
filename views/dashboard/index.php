90f2c97a717ba5d449cb2443a10afc1c95f1e8eb
eddy85bg <milushev.g@abv.bg>
2019-02-16 00:07:12 +0200

    main page color change

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,4 +1,4 @@
-<article>
+<article style="background-color:#00cc00;color:#ccffcc;">
   <h1>Bricks Core</h1>
   <p>Bricks Core is an abstract application. It lets you build and add new modules absolutely indipendent eaeach other. The proccess is the same as build a wall.</p>
 </article>
