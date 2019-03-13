ea7cd62c854ab6b74694213a5514bc75020bf888
eddy85bg <milushev.g@abv.bg>
2019-03-13 10:58:34 +0200

    Starting tests with survey form from FreeCodeCamp

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,4 +1,17 @@
-<article style="background-color:#00cc00;color:#ccffcc;">
+<article>
+  <!--
   <h1>Bricks Core</h1>
   <p>Bricks Core is an abstract application. It lets you build and add new modules absolutely indipendent eaeach other. The proccess is the same as build a wall.</p>
+  -->
+  <h1 id="title">Survey form</h1>
+  	<p id="description">Let me know your favorite music genre</p>
+  	<form id="survey-form">
+  		<label id="name-label">* Name</label>
+  		<input type="text" name="name" id="name" required placeholder="Enter your name">
+  		<label id="email-label">* Email</label>
+  		<input type="email" name="email" id="email" required placeholder="Enter your name">
+  		<label id="number-label">* Age</label>
+  		<input type="number" name="age" id="number" min="16" max="120" required placeholder="Enter your email">
+
+  		<script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
 </article>
